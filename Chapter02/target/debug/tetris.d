f7259fba35c0b973f5a395c9f651cd5d9c4db513
Saverio Miroddi <saverio.pub2@gmail.com>
2020-06-27 23:25:47 +0200

    Remove **/target directories

--- Chapter02/target/debug/tetris.d
+++
@@ -1,1 +0,0 @@
-/home/test/Documents/Mehul/Rust/ch2/target/debug/tetris: /home/test/Documents/Mehul/Rust/ch2/src/main.rs
--- Chapter09/ftp4/target/debug/deps/cfg_if-825c75be2efa57ae.d
+++
@@ -1,5 +0,0 @@
-/home/test/Documents/Mehul/Rust/Chapter 9/ftp4/target/debug/deps/libcfg_if-825c75be2efa57ae.rlib: /home/test/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.2/src/lib.rs
-
-/home/test/Documents/Mehul/Rust/Chapter 9/ftp4/target/debug/deps/cfg_if-825c75be2efa57ae.d: /home/test/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.2/src/lib.rs
-
-/home/test/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.2/src/lib.rs:
